e824c0da9345842dd79e95eebcd292e612dc266f
dunai1907 <dunai1907@gmail.com>
2020-10-31 21:35:19 +0300

    решили задачу 4.1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = friends.map(friend => (`${friend.firstName} ${friend.lastName}`));
+  let ul = document.createElement('ul');
+  for (let i =0; i < list.length; i++){
+    ul.innerHTML += '<li>' + list[i] + '</li>';
+  }
+  return ul;
 }
