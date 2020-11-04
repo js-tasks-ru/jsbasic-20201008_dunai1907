e9742aaab399b255b0e0bcc9e7f620b626b6d9a4
dunai1907 <dunai1907@gmail.com>
2020-11-04 21:58:12 +0300

    Решил задачу 5.3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  
+  let arrowRight = document.querySelector(".carousel__arrow_right");
+  let arrowLeft = document.querySelector(".carousel__arrow_left");
+  let width = document.querySelector(".carousel__inner").offsetWidth;
+  let count = 1;
+
+  arrowLeft.style.display = 'none';
+  
+  arrowRight.onclick = function(){
+    arrowLeft.style.display = '';
+    if (count==3) { 
+      arrowRight.style.display = 'none';
+    }
+    document.querySelector(".carousel__inner").style.transform = `translateX(-${count * width}px)`;
+    count++;
 }
+
+ arrowLeft.onclick = function(){
+  count--;
+  arrowRight.style.display = '';
+  if (count==1) { 
+    arrowLeft.style.display = 'none';
+  }
+    document.querySelector(".carousel__inner").style.transform = `translateX(-${(count-1)*width}px)`;
+  }
+}
+  
